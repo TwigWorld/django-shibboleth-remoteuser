2f8e389d0c513ea38a59f25b89a26ccb9fed2df9
Aaron Bassett <me@aaronbassett.com>
2013-01-18 14:23:53 +0000

    Simplify for our custom needs

--- shibboleth/middleware.py
+++ shibboleth/middleware.py
@@ -5,10 +5,9 @@ from django.core.exceptions import ImproperlyConfigured
 import hashlib
 import re
 
-#from app_settings import SHIB_ATTRIBUTE_MAP, SHIB_MOCK_HEADERS
 
 class ShibbolethRemoteUserMiddleware(RemoteUserMiddleware):
-    #From: http://code.djangoproject.com/svn/django/tags/releases/1.3/django/contrib/auth/middleware.py
+
     def process_request(self, request):
         # AuthenticationMiddleware is required so that request.user exists.
         if not hasattr(request, 'user'):
@@ -18,44 +17,28 @@ class ShibbolethRemoteUserMiddleware(RemoteUserMiddleware):
                 " MIDDLEWARE_CLASSES setting to insert"
                 " 'django.contrib.auth.middleware.AuthenticationMiddleware'"
                 " before the RemoteUserMiddleware class.")
-        try:
-            username = request.META[self.header]
-            username_regex = re.compile('[^\w.@+-]|.{31}', re.IGNORECASE)
 
+        if request.user.is_authenticated():
+            return  # Exit early is already authenticated
+
+        if "AUTH_TYPE" in request.META and request.META["AUTH_TYPE"] == "shibboleth":
+            username = request.META['persistent-id']
+
+            username_regex = re.compile('[^\w.@+-]|.{31}', re.IGNORECASE)
             if username_regex.match(username):  # invalid username
                 username = hashlib.md5(username).hexdigest()[:30]
-        except KeyError:
-            # If specified header doesn't exist then return (leaving
-            # request.user set to AnonymousUser by the
-            # AuthenticationMiddleware).
-            return
-        # If the user is already authenticated and that user is the user we are
-        # getting passed in the headers, then the correct user is already
-        # persisted in the session and we don't need to continue.
-        if request.user.is_authenticated():
-            if request.user.username == self.clean_username(username, request):
-                return
 
-        #Make sure we have all required Shiboleth elements before proceeding.
-        shib_meta, error = parse_attributes(request.META)
-        #Add parsed attributes to the session.
-        request.session['shib'] = shib_meta
-        if error:
-            raise ShibbolethValidationError("All required Shibboleth elements"
-                                            " not found.  %s" % shib_meta)
 
         # We are seeing this user for the first time in this session, attempt
         # to authenticate the user.
         user = auth.authenticate(remote_user=username)
+
         if user:
             # User is valid.  Set request.user and persist user in the session
             # by logging the user in.
             request.user = user
             auth.login(request, user)
             user.set_unusable_password()
-            user.first_name = shib_meta.get('first_name', '')
-            user.last_name = shib_meta.get('last_name', '')
-            user.email = shib_meta.get('email', '')
             user.save()
             #call make profile.
             self.make_profile(user, shib_meta)
@@ -67,24 +50,3 @@ class ShibbolethRemoteUserMiddleware(RemoteUserMiddleware):
         from the Shib provided attributes.  By default it does noting.
         """
         return
-
-
-def parse_attributes(META):
-    """
-    From: https://github.com/russell/django-shibboleth/blob/master/django_shibboleth/utils.py
-    Pull the mapped attributes from the apache headers.
-    """
-    shib_attrs = {}
-    error = False
-    for header, attr in settings.SHIBBOLETH_ATTRIBUTE_MAP.items():
-        required, name = attr
-        value = META.get(header, None)
-        shib_attrs[name] = value
-        if not value or value == '':
-            if required:
-                error = True
-    return shib_attrs, error
-
-
-class ShibbolethValidationError(Exception):
-    pass
